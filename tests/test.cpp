8bf8c9c740a0d0148b4629608d1c90fc0accb7a2
amirnyy <37066891+amirnyy@users.noreply.github.com>
2018-12-27 20:21:14 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,5 +1,9 @@
-// Copyright 2018 Your Name <your_email>
+// Copyright 2018 amirnyy <alex.mirnyy@gmail.com>
 
 #include <gtest/gtest.h>
+#include <server.hpp>
 
-#include <header.hpp>
+TEST(talk_to_client, Test2) {
+    auto talk_ptr = std::make_shared<talk_to_client>();
+    SUCCEED();
+}
